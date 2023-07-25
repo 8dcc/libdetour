553cadb985ff4b1e572b6652e8cb621e8de4f2a1
8dcc <8dcc.git@gmail.com>
2023-07-25 19:38:41 +0200

    Add main program for testing the lib

--- src/main.c
+++ src/main.c
@@ -1,32 +1,54 @@
+/**
+ * @file      main.c
+ * @brief     Example of the detour library
+ * @author    8dcc
+ */
 
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+#include "detour.h"
 
-/**
- * @brief The PI number
- */
-#define PI 3.141592
+/* Here we asociate the return type and arguments for the function "foo". This
+ * macro will create the type "foo_t", that will be used by CALL_ORIGINAL() */
+DECL_DETOUR_TYPE(bool, foo, double, double);
 
-/**
- * Simple function for adding 2 integers.
- *
- * @todo Rename function
- * @brief Sum a + b
- * @param a, b The numbers to sum
- * @return Sum of a + b
- */
-int test_func(int a, int b) {
-    return a + b;
+/* We declare the detour data struct global so it's accesible from our hook */
+detour_data_t detour_data;
+
+/* This would be the target function we want to hook */
+bool foo(double a, double b) {
+    printf("foo: %f\n", a + b);
+    return true;
 }
 
-/**
- * @todo Argument handling
- * @brief Entry point of the program
- * @param argc Number of arguments
- * @param argv Vector of string arguments
- * @return Exit code
- */
-int main(int argc, char** argv) {
-    printf("Hello, world!\n");
+/* This would be our hook, the function that we want to be called at the
+ * begining of foo(). We don't need to return anything here. */
+void hook(double a, double b) {
+    printf("hook: %f, %f\n", a, b);
+
+    /* We call the original function at the end of our hook */
+    CALL_ORIGINAL(detour_data, foo, a, b);
+}
+
+int main() {
+    void* orig_ptr = &foo;
+    void* hook_ptr = &hook;
+
+    /* Initialize detour_data struct */
+    detour_init(&detour_data, orig_ptr, hook_ptr);
+
+    /* Detour hook the original function */
+    detour_add(&detour_data);
+
+    /* Call the original function to test */
+    foo(5.0, 2.0);
+
+    /* Once we are done, we can remove the hook */
+    detour_del(&detour_data);
+
+    /* Hook is not called anymore */
+    foo(11.0, 3.0);
+
     return 0;
 }
-
