314a38310a9c995aa29495853cad00d2e4cd82d1
8dcc <8dcc.git@gmail.com>
2023-07-26 13:26:28 +0200

    Update main example to use GET_ORIGINAL
    Make foo() return a value, store it from hook(), print it

--- src/main.c
+++ src/main.c
@@ -12,24 +12,29 @@
 /* Here we asociate the return type and arguments for the function "foo". This
  * macro will create the type "foo_type_t", that will be used by the
  * CALL_ORIGINAL() macro from the hook. */
-DECL_DETOUR_TYPE(bool, foo_type, double, double);
+DECL_DETOUR_TYPE(double, foo_type, double, double);
 
 /* We declare the detour data struct global so it's accesible from our hook */
 detour_data_t detour_data;
 
 /* This would be the target function we want to hook */
-bool foo(double a, double b) {
-    printf("foo: %f\n", a + b);
-    return true;
+double foo(double a, double b) {
+    printf("foo: %.1f\n", a + b);
+    return a - b;
 }
 
 /* This would be our hook, the function that we want to be called at the
  * begining of foo(). We don't need to return anything here. */
 void hook(double a, double b) {
-    printf("hook: %f, %f\n", a, b);
+    printf("hook: %.1f, %.1f\n", a, b);
 
-    /* We call the original function at the end of our hook, with its type */
-    CALL_ORIGINAL(detour_data, foo_type, a, b);
+    /* We call the original function at the end of our hook, with its type. We
+     * use GET_ORIGINAL instead of CALL_ORIGINAL because we care about the
+     * returned value of foo(). Use CALL_ORIGNAL if you dont care about it. */
+    double ret_value;
+    GET_ORIGINAL(detour_data, ret_value, foo_type, a, b);
+
+    printf("hook: returned value is %.1f\n", ret_value);
 }
 
 int main() {
