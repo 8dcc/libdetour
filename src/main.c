031239115a84845b2cfb7a5996a1991755a8e8dd
8dcc <8dcc.git@gmail.com>
2023-12-08 16:42:48 +0100

    Update main.c example

--- src/main.c
+++ src/main.c
@@ -9,49 +9,57 @@
 #include <stdio.h>
 #include "detour.h"
 
-/* Here we asociate the return type and arguments for the function "foo". This
- * macro will create the type "foo_type_t", that will be used by the
- * CALL_ORIGINAL() macro from the hook. */
-DECL_DETOUR_TYPE(double, foo_type, double, double);
+/* Here we asociate the return type and arguments for the function `foo'. This
+ * macro will create a `typedef' that will be used internally by the macros. */
+DETOUR_DECL_TYPE(double, foo, double, double);
 
-/* We declare the detour data struct global so it's accesible from our hook */
-detour_data_t detour_data;
+/* We declare the detour context global so it's accesible from our hook */
+detour_ctx_t detour_ctx;
 
-/* This would be the target function we want to hook */
+/* Target function we want to hook */
 double foo(double a, double b) {
-    printf("foo: %.1f\n", a + b);
-    return a - b;
+    printf("foo: %.1f + %.1f = %.1f\n", a, b, a + b);
+    return a + b;
 }
 
-/* This would be our hook, the function that we want to be called at the
- * begining of foo(). We don't need to return anything here. */
+/* Our hook, the function that we want to be called instead of foo() */
 void hook(double a, double b) {
-    printf("hook: %.1f, %.1f\n", a, b);
+    printf("hook: got values %.1f and %.1f\n", a, b);
 
-    /* We call the original function at the end of our hook, with its type. We
-     * use GET_ORIGINAL instead of CALL_ORIGINAL because we care about the
-     * returned value of foo(). Use CALL_ORIGNAL if you dont care about it. */
-    double ret_value;
-    GET_ORIGINAL(detour_data, ret_value, foo_type, a, b);
+    printf("hook: calling original with custom values...\n");
 
-    printf("hook: returned value is %.1f\n", ret_value);
+    /* Call the original function, ignoring the return value */
+    DETOUR_ORIG_CALL(&detour_ctx, foo, 9.5, 1.5);
+
+    printf("hook: calling with original values...\n");
+
+    /* Call the original function, storing the return value.
+     * Note that the 3rd parameter is the one used for DETOUR_DECL_TYPE */
+    double my_var;
+    DETOUR_ORIG_GET(&detour_ctx, my_var, foo, a, b);
+
+    printf("hook: original returned %.1f\n", my_var);
 }
 
-int main() {
+int main(void) {
     void* orig_ptr = &foo;
     void* hook_ptr = &hook;
 
-    /* Initialize detour_data struct */
-    detour_init(&detour_data, orig_ptr, hook_ptr);
+    /* Initialize context */
+    detour_init(&detour_ctx, orig_ptr, hook_ptr);
 
-    /* Detour hook the original function */
-    detour_add(&detour_data);
+    /* Hook the original function */
+    detour_add(&detour_ctx);
+
+    printf("main: hooked, calling foo...\n");
 
     /* Call the original function to test */
     foo(5.0, 2.0);
 
     /* Once we are done, we can remove the hook */
-    detour_del(&detour_data);
+    detour_del(&detour_ctx);
+
+    printf("main: unhooked, calling again...\n");
 
     /* Hook is not called anymore */
     foo(11.0, 3.0);
