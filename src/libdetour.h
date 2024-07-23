f67d5bfb63337e0789c74c9a66c9aa9807843030
8dcc <8dcc.git@gmail.com>
2024-07-23 16:38:57 +0200

    Add license comments

--- src/libdetour.h
+++ src/libdetour.h
@@ -1,9 +1,20 @@
-/**
- * @file   libdetour.h
- * @brief  Detour hooking library header
- * @author 8dcc
+/*
+ * libdetour.h - Simple C/C++ library for detour hooking in Linux and Windows.
+ * See: https://github.com/8dcc/libdetour
+ * Copyright (C) 2024 8dcc
  *
- * https://github.com/8dcc/libdetour
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef LIBDETOUR_H_
